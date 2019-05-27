0ca9141383f3665265b8a94d473cce8d60960f2a
xingmimfl <506576511@qq.com>
2019-05-27 16:47:50 +0800

    add poly learning rate schedular

--- config.py
+++ config.py
@@ -5,8 +5,12 @@ CROP_SIZE = 1024
 DATA_PATH = "/media/disk1/mengfanli/data/cityscapes"
 
 RESNET_MODEL_PATH = 'resnet18-5c106cde.pth'
-MAX_EPOCHS = 100
 BATCH_SIZE = 16
 LEARNING_RATE = 2.5e-2
-WEIGH_DECAY = 1e-4
-
+MOMENTUM = 0.9
+WEIGHT_DECAY = 1e-4
+MAX_ITERS = 80000
+POWER = 0.9
+TRAIN_SAVE_INTERVAL = 100
+SUFFIX = "bisenet_20190527_version1"
+SNAPSHOT_PATH = "models"
--- train.py
+++ train.py
@@ -10,20 +10,47 @@ from config import *
 device = torch.device('cuda:%d' % device_id)
 
 def train(model, optimizer, dataloader_train):
-    for epoch in range(MAX_EPOCHS):
-        #lr = poly_lr_scheduler(optimizer, args.learning_rate, iter=epoch, max_iter=args.num_epochs)
-        for i, (images, masks) in enumerate(dataloader_train):
-            images = images.to(device) 
-            masks = masks.to(device).squeeze(t)
+    data_iter = iter(dataloader_train)
+    for epoch in range(MAX_ITERS):
+        lr = poly_lr_scheduler(optimizer, LEARNING_RATE, iter_count=epoch, max_iter=MAX_ITERS)
+        try:
+            images, masks = data_iter.next()
+        except StopIteration:
+            data_iter = iter(dataloader_train) 
+            images, masks = data_iter.next()
+        images = images.to(device) 
+        masks = masks.to(device).squeeze()
         
-            output, output_sup1, output_sup2 = model(images)
-            loss_p, loss_16, loss_32 = model.get_loss(output, output_sup1, output_sup2, masks)
-            loss = loss_p + loss_16 + loss_32
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-          
+        output, output_sup1, output_sup2 = model(images)
+        loss_p, loss_16, loss_32 = model.get_loss(output, output_sup1, output_sup2, masks)
+        loss = loss_p + loss_16 + loss_32
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
+        #----save model----
+        if epoch % TRAIN_SAVE_INTERVAL == 0:
+            save_name = '_'.join([SUFFIX, "iter", str(epoch), '.model'])
+            save_path = os.path.join(SNAPSHOT_PATH, save_name)
+            torch.save(model, save_path)
+            print("save model:\t", save_path)
+
+def poly_lr_scheduler(optimizer, init_lr, iter_count, lr_decay_iter=1,
+                      max_iter=100, power=0.9):
+    """Polynomial decay of learning rate
+        :param init_lr is base learning rate
+        :param iter is a current iteration
+        :param lr_decay_iter how frequently decay occurs, default is 1
+        :param max_iter is number of maximum iterations
+        :param power is a polymomial power
+
+    """
+    lr = init_lr*(1 - iter_count/max_iter)**power
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    return lr
+          
 
 if __name__=="__main__":
     #----init dataloader----
@@ -51,9 +78,15 @@ if __name__=="__main__":
             resnet_params.append(param)
         else:
             model_other_params.append(param)
-    optimizer = torch.optim.RMSprop([
-        {'params':resnet_params, 'lr':LEARNING_RATE * 0.1},
-        {'params':model_other_params}],
-        lr = LEARNING_RATE)
+
+    optimizer = torch.optim.SGD(
+        [
+            {'params':resnet_params, 'lr':LEARNING_RATE * 0.1},
+            {'params':model_other_params}
+        ],
+        lr = LEARNING_RATE, 
+        momentum = MOMENTUM,
+        weight_decay = WEIGHT_DECAY,
+    )
         
     train(model, optimizer, train_dataloader) 
